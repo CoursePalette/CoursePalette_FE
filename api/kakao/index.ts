dcbcca26b884b0ac0274928e8a09f3cc36a989be
99andrew99 <whalstjr199927@gmail.com>
2025-03-29 20:12:22 +0900

    [feat] 카카오 로그인 구현 완료

---
+++ api/kakao/index.ts
@@ -0,0 +1,35 @@
+import { signIn, signOut } from 'next-auth/react';
+import { axiosInstance } from '../axiosInstance';
+
+export interface sendKakaoProfileResponse {
+  token: string; // 백엔드 JWT
+  nickname: string;
+  userId: number;
+  profileImageUrl: string;
+}
+
+export const sendKakaoProfile = async(
+  kakaoId: number,
+  nickname: string,
+  profileImageUrl: string,
+) : Promise<sendKakaoProfileResponse> => {
+  const response = await axiosInstance.post('/auth/kakao', {
+    kakaoId,
+    nickname,
+    profileImageUrl,
+  });
+
+  return response.data;
+}
+
+export const kakaoLogin = async() => {
+  await signIn('kakao');
+}
+
+export const kakaoLogout = async() => {
+  await signOut(
+    {
+      callbackUrl: "/"
+    }
+  )
+};
