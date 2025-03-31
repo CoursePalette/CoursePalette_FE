1a9e81a7be819346a3bf4088d99702df6a835aaa
99andrew99 <whalstjr199927@gmail.com>
2025-03-31 21:13:44 +0900

    [feat] 헤더 라우터 로직 추가

--- components/atoms/Header.tsx
+++ components/atoms/Header.tsx
@@ -3,16 +3,41 @@
 import { IoMenu } from 'react-icons/io5';
 import { TiArrowLeft } from 'react-icons/ti';
 
+import { useRouter } from 'next/navigation';
+
+import { useCallback } from 'react';
+
 export default function Header() {
+  const router = useRouter();
+
+  const handleBack = useCallback(() => {
+    router.back();
+  }, [router]);
+
+  const handleHome = useCallback(() => {
+    router.replace('/');
+  }, [router]);
+
+  const handleMenu = useCallback(() => {
+    router.push('/menu');
+  }, [router]);
+
   return (
     <div className='w-full h-[50px] bg-white/80 backdrop-blur-lg border-b border-black flex items-center px-[20px] justify-between fixed top-0 left-0'>
       <div className='flex gap-[20px] items-center'>
-        <TiArrowLeft className='cursor-pointer' size={35} />
-        <span className='cursor-pointer font-semibold text-[16px]'>
+        <TiArrowLeft
+          className='cursor-pointer'
+          size={35}
+          onClick={handleBack}
+        />
+        <span
+          className='cursor-pointer font-semibold text-[16px]'
+          onClick={handleHome}
+        >
           코스팔레트
         </span>
       </div>
-      <IoMenu className='cursor-pointer' size={30} />
+      <IoMenu className='cursor-pointer' size={30} onClick={handleMenu} />
     </div>
   );
 }
