02db76f1c572fc6e2f86e67adb760f791d125497
99andrew99 <whalstjr199927@gmail.com>
2025-03-31 20:05:07 +0900

    [refactor] 메타 태그 서버 컴포넌트로 전환

---
+++ app/course/create/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: '코스 생성하기 | 코스팔레트',
+  description: '코스를 직접 등록하고 공유하세요!',
+};
+
+export default function CourseCreateLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
