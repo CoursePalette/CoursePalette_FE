2975d8c8a0e80490c1b6a439cbab4ad2b411a07f
99andrew99 <whalstjr199927@gmail.com>
2025-04-02 12:18:22 +0900

    [feat] 코스 생성 버튼 구현

---
+++ components/atoms/CourseCreateButton.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { FaPlus } from 'react-icons/fa';
+
+import { Button } from '../ui/button';
+
+export default function CourseCreateButton() {
+  return (
+    <Button
+      className='!w-[50px] !h-[50px] md:!w-[75px] md:!h-[75px] bg-[#0064FF] hover:bg-[#D3E4FF] text-white hover:text-[#0064FF] flex items-center justify-center
+      fixed bottom-[60px] right-[20px] md:bottom-[60px] md:right-[55px] rounded-full
+    '
+    >
+      <FaPlus className='!w-[24px] !h-[24px] md:!w-[30px] md:!h-[30px]' />
+    </Button>
+  );
+}
