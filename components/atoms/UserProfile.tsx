5f96a7104b7b481bab22d72468ccf8b719b4192f
99andrew99 <whalstjr199927@gmail.com>
2025-04-04 11:28:44 +0900

    [feat] 로그인 및 로그아웃 기능 연동

---
+++ components/atoms/UserProfile.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+
+interface UserProfileProps {
+  nickname: string;
+  profileImageUrl: string;
+}
+
+export default function UserProfile({
+  nickname,
+  profileImageUrl,
+}: UserProfileProps) {
+  return (
+    <section className='flex items-center gap-[7px]'>
+      <Image
+        src={profileImageUrl}
+        alt='유저 프로필 사진'
+        width={30}
+        height={30}
+        className='rounded-[50px]'
+        aria-label='유저 프로필 사진'
+      />
+      <span className='text-[14px] font-medium' aria-label='유저 닉네임'>
+        {nickname}
+      </span>
+    </section>
+  );
+}
