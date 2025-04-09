445fe92ff258253f5f0ce77556642cf699b43a89
99andrew99 <whalstjr199927@gmail.com>
2025-04-09 13:59:00 +0900

    [test] FavoriteButton

--- __mocks__/next/navigation.ts
+++ __mocks__/next/navigation.ts
@@ -2,6 +2,10 @@ export const useRouter = () => {
   return {
     push: () => console.log('[mock] push'),
     back: () => console.log('[mock] back'),
+    replace: () => console.log('[mock] replace'),
+    prefetch: () => console.log('[mock] prefetch'),
+    forward: () => console.log('[mock] forward'),
+    refresh: () => console.log('[mock] refresh'),
     // 여기에 필요한 라우터 기능들 계속 mock
   };
 };
