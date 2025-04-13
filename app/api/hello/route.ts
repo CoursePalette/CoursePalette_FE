27945ffe2e5e8ecff414b3256fb79bd333172e05
99andrew99 <whalstjr199927@gmail.com>
2025-04-13 12:51:44 +0900

    [test] test

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-export async function GET(_request: Request) {
+export async function GET() {
   console.log('>>> HELLO API ROUTE CALLED - Vercel <<<'); // Function Log 확인용
   return NextResponse.json({ message: 'Hello from Vercel!' });
 }
