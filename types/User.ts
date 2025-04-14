358f63cea4c2738b02083ffb3f5e2bd8c62904d1
99andrew99 <whalstjr199927@gmail.com>
2025-04-14 14:32:55 +0900

    [feat] 유저 업데이트 api 연결

--- types/User.ts
+++ types/User.ts
@@ -10,3 +10,13 @@ export interface KakaoAuthResponseDto {
   nickname: string;
   profileImageUrl: string;
 }
+
+// 사용자 프로필 업데이트 요청시 타입
+export interface UpdateProfileRequest {
+  nickname: string;
+  profileImageUrl: string;
+}
+
+export interface UpdateProfileResponse {
+  message: string;
+}
